0db795839e80e640b8724222389e82b21e2346f8
Kalyan-1403 <238b1a0547@risekrishnasaigandhi.edu.in>
2026-04-19 17:20:47 +0530

    Hod dashboar modified

--- Frontend/src/components/HoDDashboard/HoDDashboard.jsx
+++ Frontend/src/components/HoDDashboard/HoDDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, useCallback } from 'react';
+import React, { useState, useMemo, useEffect, useCallback, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import dataService from '../../services/dataService.js';
